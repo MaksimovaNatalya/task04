94e830ee4ebfea52913892b96d299072d84ad43a
MaksimovaNatalya <ps_nat98@mail.ru>
2021-12-10 22:24:02 +0300

    started Controller

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Controller extends HttpServlet {
+
+    public Controller() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      process();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process();
+    }
+
+    private void process() {
+        System.out.println("Hello");
+    }
+
+
+}
